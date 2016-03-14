f9b719124fe343e07d982f080a5995e65276319b
Камиль Гатауллин <gataullin.kamil@gmail.com>
2016-03-14 23:50:38 +0300

    added Spring form validation for registration form

--- public/src/main/java/com/springapp/mvc/controllers/RegistrationController.java
+++ public/src/main/java/com/springapp/mvc/controllers/RegistrationController.java
@@ -1,13 +1,17 @@
 package com.springapp.mvc.controllers;
 
 import com.springapp.mvc.common.MenuInfo;
+import com.springapp.mvc.form.RegistrationFormBean;
 import com.springapp.mvc.services.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -28,11 +32,24 @@ public class RegistrationController {
         // TODO вынести в аспект
         List<MenuInfo> listMenu = menuService.getMainMenu();
         request.setAttribute("listMenu", listMenu);
+
+        request.setAttribute("regForm", new RegistrationFormBean());
         return "registration/registrationPage";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String registrationForm() {
-        return "registration/registrationPage";
+    public String registrationForm(
+            @Valid @ModelAttribute("regForm") RegistrationFormBean registrationFormBean,
+//            RegistrationFormBean registrationFormBean, // TODO хотя работает и без этой аннотации, обычно её используют для переименования аргумента
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // TODO вынести в аспект
+            List<MenuInfo> listMenu = menuService.getMainMenu();
+            request.setAttribute("listMenu", listMenu);
+            return "registration/registrationPage";
+        }
+        // здесь должна происходить регистрация пользователя
+        System.out.println(registrationFormBean);
+        return "registration/result";
     }
 }
